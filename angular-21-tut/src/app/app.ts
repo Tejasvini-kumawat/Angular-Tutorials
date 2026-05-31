c964755c3a0370e855d9a373a625327a04cd9aaf
Tejasvini Kumawat <tejasvinikumawat2004@gmail.com>
2026-05-31 23:25:39 +0530

    Added Registration form component

--- angular-21-tut/src/app/app.ts
+++ angular-21-tut/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login';
+import { Registration } from './registration/registration';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Login],
+  imports: [RouterOutlet,Login,Registration],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
